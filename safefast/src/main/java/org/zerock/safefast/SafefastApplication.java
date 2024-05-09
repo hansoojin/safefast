87509a9b74683b15f16939d223ebbbb7bf9692ab
songoffeather <kaellyur@gmail.com>
2024-05-09 16:36:31 +0900

    hello, world

---
+++ safefast/src/main/java/org/zerock/safefast/SafefastApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.safefast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafefastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafefastApplication.class, args);
+    }
+
+}
